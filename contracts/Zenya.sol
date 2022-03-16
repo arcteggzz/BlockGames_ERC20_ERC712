f9f624ced575bbf949e42dc61c04cfab12477cc0
"Esedere Oghenetega " <"arcteggzz@gmail.com">
2022-03-16 12:55:25 +0100

    Complete Contract

--- contracts/Zenya.sol
+++ contracts/Zenya.sol
@@ -7,22 +7,43 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Zenya is ERC20, Ownable {
 
     //state variable
-    // uint256 public totalSupply_;
-    string public standardPrefix = "Your total supply is ";
+    uint public _totalSupply = 1000000;
+    // uint256 public __totalSupply;
+    // mapping (address => uint256) public __balanceOf;
+    // string public standardPrefix = "Your total supply is ";
+    // string public standardSuffix = " ether";
 
     constructor() ERC20("Zenya", "ZNY") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
+    
+    // function totalSupply()  public returns (uint256 _totalSupply) {
+    //     __totalSupply = 1000000;
+    //     _totalSupply = __totalSupply;
+    // }
+
+    
+    // function balanceOf(address addr) public view returns (uint256 balance){
+    //     return __balanceOf[addr];
+    // }
 
     // function printTotalSupply() public view returns (string memory){
-    //     return string(abi.encodePacked(standardPrefix, totalSupply));
+    //     return string(abi.encodePacked(standardPrefix, totalSupply_, standardSuffix));
     // }
 
-    // function mint(address to, uint256 amount) public onlyOwner {
-    //     _mint(to, amount);
-    // } 
+    function buyToken(address receiver, uint256 amountPurchased) public payable{
+        
+        //create a variable to track the fee of purchase
+        uint256 fee = amountPurchased * 1000;
 
-    // function setTotalSupply()  public {
-    //     totalSupply_ = 1000000;
-    // }
+        uint256 balance_ =  balanceOf(receiver);
+        //reduce token balance of receiver by fee
+        balance_ -= fee;
+
+        //increase token balance of receiver by tokens bought
+        balance_ += amountPurchased;
+
+        //increase total supply by tokens bought
+        _totalSupply = _totalSupply + amountPurchased;
+    }
 }
